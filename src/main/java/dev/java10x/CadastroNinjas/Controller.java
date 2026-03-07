7175ccb86e62fb22736ee518e3d5035ffe528024
Cicero Gledson <cicero.gledson@hotmail.com>
2026-03-07 14:16:46 -0300

    Criada a controller [aula2]

---
+++ src/main/java/dev/java10x/CadastroNinjas/Controller.java
@@ -0,0 +1,14 @@
+package dev.java10x.CadastroNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/helloWorld")
+    public String helloWorld(){
+        return "Hellow world!";
+    }
+}
