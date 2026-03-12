ac40f3f3664119792ffd1768f0574fd2025338c0
Cicero Gledson <cicero.gledson@hotmail.com>
2026-03-11 22:54:14 -0300

    feat: Cria as rotas do controller das missões com mock

---
+++ src/main/java/dev/java10x/CadastroNinjas/Missoes/MissoesController.java
@@ -0,0 +1,32 @@
+package dev.java10x.CadastroNinjas.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Lista de missões"
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missão alterada com sucesso";
+    }
+    @DeleteMapping("/deletar")
+    public String deletarMissao (){
+        return "Missão deletada com sucesso";
+    }
+
+
+}
+
+
+
